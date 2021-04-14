aab0badb69ca3f4de89084904afca0993738af51
Akshat Mahajan <akshatm.bkk@gmail.com>
2021-04-13 20:42:28 -0400

    Update broken benchmark + port to Python 3.6
    When I run the script with the default Python3 shipped in the class Vagrant, I get the following error:
    
    ```
    Traceback (most recent call last):
      File "bench.py", line 87, in <module>
        main()
      File "bench.py", line 79, in main
        results[name] = bench(config[name])
      File "bench.py", line 49, in bench
        start = time.time_ns()
    AttributeError: module 'time' has no attribute 'time_ns'
    ```
    
    It appears nanosecond-precision was only added to Python in 3.7, but the class Vagrant ships with Python 3.6.9 (so close!)
    
    After trying for a while to move to Python 3.9 inside the Vagrant environment, I gave up and just replaced
    ```
            for i in range(N):
                start = time.time_ns()
                subprocess.run(
                    run_cmd, input=stdin, shell=True, capture_output=True, text=True
                )
                end = time.time_ns()
                runs.append(end - start)
    ```
    with
    ```
            for i in range(N):
                start = time.perf_counter()
                subprocess.run(
                    run_cmd, input=stdin, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                    universal_newlines=True
                )
                end = time.perf_counter()
                runs.append((end - start) * 10 ** 9)
    ```
    
    This is sufficient to get the whole thing to work under Python3.6 and should spit equivalent numbers (albeit in floating point, but you still get it at nanosecond precision).
    
    Technically, capture_output is not necessary at all in our script as we never read the stdout or stderr produced by us. I ported it anyway.
    
    Also, minor feedback: the CSV result is pretty confusing. By changing the last few lines in bench.py to
    
    ```
        for benchmark in sorted(results[name].keys()):
            for name in sorted(results.keys()):
                writer.writerow((benchmark, name, results[name][benchmark]))
    ```
    
    I was able to make it much easier to compare the impact of different configured optimizations on a single file. Now the output is ordered by the file name, rather than by the configuration option.
    
    Example output for my branch (which is public to everyone in the class in the PR filed against the benchmarks, so I feel it is okay reproducing here):
    
    ```
    constant_propagation.lisp,Constant propagation and inlining,1827865.100085546
    constant_propagation.lisp,Inlining,2280421.400018895
    constant_propagation.lisp,No optimizations,2421028.1999330623
    example.lisp,Constant propagation and inlining,1948288.299945489
    example.lisp,Inlining,1943638.7000041578
    example.lisp,No optimizations,2178377.6999654947
    inlining-with-constant-propagation.lisp,Constant propagation and inlining,1936039.7000127707
    inlining-with-constant-propagation.lisp,Inlining,2034152.300120695
    inlining-with-constant-propagation.lisp,No optimizations,2219833.1001618495
    simple-inlining.lisp,Constant propagation and inlining,1975962.8999963752
    simple-inlining.lisp,Inlining,2396916.000043348
    simple-inlining.lisp,No optimizations,2328277.7000531494
    ```

--- benchmarks/simple-inlining.lisp
+++ benchmarks/simple-inlining.lisp
@@ -1,3 +1,15 @@
 (define (minimum x y) (if (< x y) x y))
 
-(print (+ (minimum 0 1) (+ (+ (minimum 1 2) (+ (+ (minimum 3 4) (+ (+ (minimum 5 6) 0) 7)))))))
+(print 
+    (+ (minimum 0 1) 
+        (+ (minimum 1 2)
+            (+ (minimum 2 3)
+                (+ (minimum 3 4)
+                    (+ (minimum 4 5)
+                        (+ (minimum 5 6) 7)
+                    )
+                )
+            )
+        )
+    )
+)
