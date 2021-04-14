096498e222f435ee661abe9dbc5b68155c9c5eb0
Teresa Amor <teresa_amor@brown.edu>
2021-04-14 23:11:25 +0000

    benchmarks

---
+++ benchmarks/constant_prop_let.lisp
@@ -0,0 +1,8 @@
+(let ((a 1))
+    (let ((b 2))
+        (let ((c 3))
+            (let ((d 4))
+                (if (+ a (+ b (+ c (+ d d))))
+                    (print (+ a (+ b (+ c (+ d d)))))
+                    (print (+ a (+ b (+ c (+ d d)))))
+                    )))))
