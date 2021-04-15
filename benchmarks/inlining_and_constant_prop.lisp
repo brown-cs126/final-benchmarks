3a6e3fdb3fd56844ca7e7aaa90857ca1657f0c26
wbuerger46 <william_buerger@brown.edu>
2021-04-15 00:24:59 +0000

    Fixed parse error

--- benchmarks/inlining_and_constant_prop.lisp
+++ benchmarks/inlining_and_constant_prop.lisp
@@ -2,5 +2,5 @@
     (- (+ x (add1 y)) (+ (add1 x) (sub1 y))))
 
 (let ((x (f 0 0)))
-    (let ((y (f x 2))))
-        (print (f y (read-num))))
+    (let ((y (f x 2)))
+        (print (f y (read-num)))))
