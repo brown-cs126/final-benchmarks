09c529b500ed1198c7df90688578ceef73eda8e8
iambadatnames <charles_cutting@brown.edu>
2021-04-20 20:55:22 -0400

    ryan's benchmarks

--- benchmarks/inlining.lisp
+++
@@ -1,9 +0,0 @@
-(define (unnecessary-1 x) (sub1 x))
-(define (unnecessary-2 x) (unnecessary-1 x))
-(define (unnecessary-3 x) (unnecessary-2 x))
-(define (unnecessary-4 x) (unnecessary-3 x))
-(define (unnecessary-5 x) (unnecessary-4 x))
-
-(define (foo x) (if (< x 1) x (foo (unnecessary-5 x))))
-
-(foo 100000)
--- benchmarks/unique_names.lisp
+++
@@ -1,3 +0,0 @@
-(define (count-down x y z) (if (< x y) z (count-down (sub1 x) y (add1 z))))
-(define (foo x y) (count-down x y 0))
-(let ((x 10)) (let ((y 50)) (foo y x)))
