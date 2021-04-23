68647c7baad16e0a121c91193036766585ea1453
yash <me@me.com>
2021-04-23 21:11:38 +0000

    benchmarks

---
+++ benchmarks/inlin.lisp
@@ -0,0 +1,12 @@
+
+(define 
+    (f x y) 
+    (+ x y))
+
+(print 
+    (f 
+        (f 
+            (f 
+                (f 
+                    (f x 
+                        (f x y)) y) y) y) y) 
