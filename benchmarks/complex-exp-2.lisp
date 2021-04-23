d937e37e424b8201c44ae53e34c1eaf3dc0a7a3c
Jed Fox <git@jedfox.com>
2021-04-23 14:13:51 -0400

    Add 3 benchmarks

---
+++ benchmarks/complex-exp-2.lisp
@@ -0,0 +1,7 @@
+(print (pair
+    (empty?
+        (if (zero? (not (let ((V1 (not (pair (- 35 35) (pair 79 (if (not false) 70 8))))))
+                (right (pair (sub1 9) (not (let ((v1 ())) (let ((v1 (zero? ()))) (zero? (sub1 80))))))))))
+            false
+            (not false)))
+    (pair 9 (pair (+ (add1 32) 80) ()))))
---
+++ benchmarks/complex-exp.lisp
@@ -0,0 +1,4 @@
+(print (let ((V1 (add1 99)))
+    (if (pair? (left (pair (sub1 48) 56)))
+        true
+        (pair? (if false (- 47 20) 80)))))
